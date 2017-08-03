05c40de44dc2a407baa46a94d5ca4abcffc67c28
Liso Gallo <liso@riseup.net>
2017-08-03 13:32:47 -0300

    [ADD] Resource fields and views

--- qms/models/resource.py
+++ qms/models/resource.py
@@ -7,4 +7,26 @@ class Resource(models.Model):
 
     _name = "qms.resource"
 
-    name = fields.Char()
+    _resource_types_ = [
+        ('internal', 'Internal'),
+        ('external', 'External')
+    ]
+
+    _resource_states_ = [
+        ('available', 'Available'),
+        ('in_process', 'In Process'),
+        ('not_available', 'Not Available')
+    ]
+
+    name = fields.Char(
+        required=True
+    )
+
+    resource_type = fields.Selection(
+        selection=_resource_types_
+    )
+
+    state = fields.Selection(
+        selection=_resource_states_,
+        default='available'
+    )
