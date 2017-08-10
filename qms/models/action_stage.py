a579313af21498dd223b65f89b92be4af9b656ef
Liso Gallo <liso@riseup.net>
2017-08-10 10:12:47 -0300

    [IMP] Action stage

--- qms/models/action_stage.py
+++ qms/models/action_stage.py
@@ -4,24 +4,9 @@ from odoo import fields, models
 
 
 class Action_Stage(models.Model):
-    """This object is used to defined different stage for actions."""
 
     _name = 'qms.action.stage'
-    _description = "Action Stages"
-    _order = 'sequence'
-
-    name = fields.Char(
-        required=True
-    )
-
-    sequence = fields.Integer(
-        help="Used to order stages. Lower is better.",
-        default=10
-    )
-
-    is_starting = fields.Boolean(
-        string='Starting stage'
-    )
+    _inherit = ['qms.stage']
 
     is_ending = fields.Boolean(
         string='Ending stage'
